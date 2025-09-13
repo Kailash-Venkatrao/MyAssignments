d231b28b1bd20f1a5c1224168362014e3a1f95ad
Kailash Venkatrao <kailashvenkat1969@gmail.com>
2025-09-13 20:58:29 +0530

    Add files via upload
    Day 01 assignments submitted.

---
+++ assignments/Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1.assignments;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer= "Google";
+		Boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println("A).Version: "+ version);
+		System.out.println("B).Developer: " + developer);
+		System.out.println("C).isBeta: "+ isBeta);
+		System.out.println("D).Release Year: " + releaseYear);
+		System.out.println("E).Shortcut Key: " + shortcutKey);
+	}
+}
